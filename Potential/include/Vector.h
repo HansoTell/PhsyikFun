ec959a706ad0a873c49bb851b6891dff52bc561e
Janis Rolf <janis.rolf@stud.uni-goettingen.de>
2026-04-19 21:09:29 +0200

    expanded vector class

--- Potential/include/Vector.h
+++ Potential/include/Vector.h
@@ -10,10 +10,20 @@ class Vector
 public:
     double& at ( size_t i ) { return data.at(i); }
     const double& at( size_t i ) const { return data.at(i); }
+    void minus( const Vector<Dim> other ) { for( int i = 0; i < Dim; i++ ) at(i) -= other[i]; }
+    void add( const Vector<Dim> other ) { for( int i = 0; i < Dim; i++ ) at(i) += other[i]; }
+    void skalarProduct( double skalar ) { for( int i = 0; i < Dim; i++ ) at(i) *= skalar; }
     double& operator[]( size_t i ) { return at(i); }
     const double& operator[]( size_t i ) const { return at(i); }
+    void operator-(const Vector<Dim>& other) { minus(other); }
+    void operator+(const Vector<Dim>& other) { add(other); }
+    void operator*( double skalar ) { skalarProduct(skalar); }
 private:
     std::array<double, Dim> data;
 };
+
+
+using Vec2D = Vector<2>;
+using Vec3D = Vector<3>;
 
 }
