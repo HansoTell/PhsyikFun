5ea8e40f69ff5a7749d0cdae91dc7683669d71a9
Janis Rolf <janis.rolf@stud.uni-goettingen.de>
2026-05-21 22:57:32 +0200

    started python plot skript

--- apps/mainPlot.py
+++ apps/mainPlot.py
@@ -1,11 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import pandas as pd
 
 mpl.use("TkAgg")
 
 
 data = np.loadtxt("../build/apps/data.csv", delimiter=",")
+
+def PrintTrajek(fileName):
+    df = pd.read_csv("")
+    indexes = df["index"].unique()
+    EntitysDfs = []
+    plt.figure() #muss 3d sein
+    for i in indexes:
+        EntitysDfs.append(df.where(df["index"] == i))
+
 
 entitys = np.unique(data[:,0])
 
