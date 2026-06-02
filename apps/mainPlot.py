ee1273e03c079d46627c104c8360b111c2eea9e1
Janis Rolf <janis.rolf@stud.uni-goettingen.de>
2026-06-02 15:47:42 +0200

    Fixed Printing now works gravitation works last step adding another bsp

--- apps/mainPlot.py
+++ apps/mainPlot.py
@@ -5,53 +5,59 @@ import pandas as pd
 
 mpl.use("TkAgg")
 
-def PrintTrajek(dataFrame):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
+def PrintTrajek(dataFrame, options):
+    for OptionsPack in options:
+        if "Trajektorie" in OptionsPack:
+            fig = plt.figure()
+            ax = fig.add_subplot(projection="3d")
 
-    idx = dataFrame["index"].unique()
-    
-    for i in idx:
-       dfi = dataFrame.where(i == dataFrame["index"])
-       xPos = dfi["pos_x"]
-       yPos = dfi["pos_y"]
-       zPos = dfi["pos_z"]
+            idx = dataFrame["index"].unique()
+            
+            for i in idx:
+               dfi = dataFrame[i == dataFrame["index"]]
+               xPos = dfi["pos_x"]
+               yPos = dfi["pos_y"]
+               zPos = dfi["pos_z"]
 
-       ax.plot(xPos,yPos,zPos)
+               ax.plot(xPos,yPos,zPos)
 
-    ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
-    fig.suptitle("Trajek")
+            ax.set_xlabel("x")
+            ax.set_ylabel("y")
+            ax.set_zlabel("z")
+            fig.suptitle("Trajektorie")
+
+# wie könnte man einstellen, dass entweder einzelnd für entitys oder ob für alle entitys bündelt
+def PrintEnergie(df:pd.DataFrame, options):
+    idx = df["index"].unique()
+
+    for OptionsPack in options:
+        if "Eges" in OptionsPack or "EKin" in OptionsPack or "EPot" in OptionsPack:
+            plt.figure()
+            for i in idx:
+                dfi = df[i == df["index"]]
+                time = dfi["Time"]
+                ekin = dfi["Ekin"]
+                epot = dfi["EPot"]
+                if "Eges" in OptionsPack:
+                    eges = ekin + epot 
+                    plt.plot(time, eges, label = f"Eges of Entity = {i}")
+                if "EKin" in OptionsPack:
+                    plt.plot(time, ekin, label = f"EKin of Entity = {i}")
+                if "EPot" in OptionsPack:
+                    plt.plot(time, epot, label = f"EPotof Entity = {i}")
+            plt.xlabel("Time")
+            plt.ylabel("E / [J]")
+            plt.title(", ".join(OptionsPack))
+            plt.legend()
+            plt.grid(True)
+
+def Plot(FilePath, options):
+    df = pd.read_csv(FilePath, delimiter=",")
+    PrintTrajek(df, options)
+    PrintEnergie(df, options)
 
     plt.show()
-
-def PlotData(x_col, y_col, xlabels, ylabels, title = None):
-    plt.figure(figsize=(10, 6))
-
-    for (i, lbl) in zip(y_col, ylabels):
-        plt.plot(x_col, i, label = lbl )
-    plt.xlabel(xlabels)
-    plt.ylabel("Value")
-    plt.title(title or f"{', '.join(ylabels)} over {xlabels}")
-    plt.legend()
-    plt.grid(True)
-    plt.ticklabel_format(useOffset=False, style='plain')
-    plt.show()
-
-def Plot3D(firsts, secounds, thirds, labels, title = None):
-    pass
-
-
-def Print(FilePath):
-    df = pd.read_csv(FilePath)
-
-    PrintTrajek(df)
-    PlotData(df["Time"], [df["Ekin"]], "Time / [s]", ["Ekin"], "Kinetic Energy")
-    PlotData(df["Time"], [df["EPot"]], "Time / [s]", ["Epot"], "Potential Energy")
-    PlotData(df["Time"], [df["Ekin"]+df["EPot"]], "Time / [s]", ["Eges"], "Energy Plot")
-
 
 
 if __name__ == "__main__":
-    Print("../build/apps/data.csv")
+    Plot("../build/apps/data.csv", [["Trajektorie"], ["Eges"], ["EKin", "EPot"]])
