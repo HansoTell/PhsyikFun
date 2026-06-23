238434fd888c0b377357c654bba2ebdeda8d8d29
Janis Rolf <janis.rolf@stud.uni-goettingen.de>
2026-06-23 19:49:10 +0200

    started with advanced kollision system

--- include/Impact.h
+++ include/Impact.h
@@ -2,6 +2,9 @@
 
 #include "Entity.h"
 #include "Vector.h"
+#include <cstddef>
+#include <cstdint>
+#include <map>
 #include <vector>
 
 namespace Physik
@@ -30,4 +33,29 @@ private:
     Vec3D CalcVelocityTangential( const Vec3D& veclocity, const Vec3D& normal ) const;
     double CalcVelocAfter( const ClassicEntity& target, const Vec3D& targetNormal, const ClassicEntity& other, const Vec3D& otherNormal ) const;
 };
+
+class AdvancedElasticImpact : public IImpactEvaluator 
+{
+    struct StateKoords
+    {
+        int32_t x_Koord, y_Koord, z_Koord;
+    };
+    struct StateKoordsHash
+    {
+        std::size_t operator() ( const StateKoords& key ) const 
+        {
+            return 1;
+        }
+    };
+public:
+    void ApplyImpacts( SimulationState& state );
+public:
+    AdvancedElasticImpact( const std::vector<const ClassicEntity>& entitys ); 
+    AdvancedElasticImpact( const AdvancedElasticImpact& ) = default;
+    AdvancedElasticImpact( AdvancedElasticImpact&& ) = default;
+    ~AdvancedElasticImpact() = default;
+private:
+    const std::vector<const ClassicEntity>& m_Entitys;
+    std::map<StateKoords, ClassicEntity*> m_Map;
+}; 
 }
---
+++ src/Impact/AdvancedElasticImpact.cpp
@@ -0,0 +1,13 @@
+#include "Impact.h"
+
+namespace Physik 
+{
+//Wie instanziert man die map richtig ig müssen ahs doer so definieren hahah miesbock darauf
+AdvancedElasticImpact::AdvancedElasticImpact( const std::vector<const ClassicEntity>& entitys ) : m_Entitys(entitys) {}
+
+void AdvancedElasticImpact::ApplyImpacts( SimulationState& state )
+{
+
+}
+    
+}
